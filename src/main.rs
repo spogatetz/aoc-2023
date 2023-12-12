583aad81b3c118cd18fe7937ea95f7f400bb812a
Scott Pogatetz <spogatetz@gmail.com>
2023-12-11 20:09:19 -0600

    regorg

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::env;
+pub mod day1;
 
 fn main() {
-    let mut sum = 0;
-    for line in read_lines("./src/day1/input.txt").unwrap() {
-        let line_string = line.unwrap();
-        let mut found_first = false;
-        let mut first: u32 = 0;
-        let mut last: u32 = 0;
-        for character in line_string.chars(){
-            if let Some(unwrapped_char) = character.to_digit(10){
-                if !found_first {
-                    first = unwrapped_char;
-                    found_first = true;
-                }
-                last = unwrapped_char
-            }
-        }
-        sum += first * 10 + last;
+    let day = env::args().collect::<Vec<String>>()[1].parse::<i32>().unwrap();
+    println!("Running solution for day {}", day);
+
+    match day{
+        1 => println!("Solution: {}", day1::solve()),
+        _ => { println!("Unknown day")}
     }
-    println!("{}", sum);
 }
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
